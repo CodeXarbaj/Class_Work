7937f2d1f1e1d1f065c42244612eda39d0c85aa4
CodeXarbaj <arbajali7650@gmail.com>
2026-04-17 10:53:35 +0530

    done

---
+++ 10/04/area_perimeter_circle.py
@@ -0,0 +1,14 @@
+# program to find area and perimeter of a circle
+
+# input radius
+r = float(input("Enter radius: "))
+
+# calculate area
+area = 3.14 * r * r
+
+#perimetr formula
+perimeter = 2 * 3.14 * r
+
+# print results
+print("Area of circle =", area)
+print("Perimeter of circle =", perimeter)
