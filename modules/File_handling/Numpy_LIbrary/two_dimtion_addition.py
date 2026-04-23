3d84f671aac0d82c9fb061ba50aaeaa5bac9b580
CodeXarbaj <arbajali7650@gmail.com>
2026-04-23 10:47:22 +0530

    done

--- modules/File_handling/Numpy_LIbrary/two_dimtion_addition.py
+++ modules/File_handling/Numpy_LIbrary/two_dimtion_addition.py
@@ -1,18 +1,21 @@
+#importing numpy library as np
 import numpy as np 
 #first array
 array1 = np.array([90,50,40,70])
 #second array
 array2 = np.array([10,20,30,60])
+
 #---------------------------------------
 print(array1)
 print("---------------------------------------")
 print(array2)
 print("----------------------------------------")
-#sum of two array
-array3 = np.add(array1,array2)
-print(array3)
 
+#sum of two array
+arrayadd = np.add(array1,array2)
+print(arrayadd)
 print("-----------------------------------------------")
+
 print("Substraction of two dimentional array")
 arraysub = np.subtract(array1,array2)       #Using sunstract function for calculating diffrence of two array
 print(arraysub)
